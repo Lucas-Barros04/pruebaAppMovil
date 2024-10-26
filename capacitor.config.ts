087966a43df50652c56242bd6a6396c9b8818419
POAPCA02LC0101 <SSDD@POAPCA02LC0101.duoc.acad>
2024-10-26 09:35:20 -0300

    Inicio de página

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lucasBarros21660978-6',
+  webDir: 'www'
+};
+
+export default config;
